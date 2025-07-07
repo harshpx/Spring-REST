fbae83e868d76a1e52f117d1ca997bbc4ed34e2f
Priye <harsh.priye@ing.com>
2025-07-07 16:56:59 +0530

    added test comment

---
+++ src/main/java/com/learn/RestWithDatabase/dto/EmployeeDTO.java
@@ -0,0 +1,21 @@
+package com.learn.RestWithDatabase.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDTO {
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	public EmployeeDTO() {};
+	public EmployeeDTO(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+}
+
+// test comment
