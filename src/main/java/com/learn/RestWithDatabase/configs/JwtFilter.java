04c8d152b90210ddd650463f6ae4361ea698a1da
harshpx <harsh.rzf@gmail.com>
2025-07-21 03:00:17 +0530

    Handeled expired token / unauthorized token requests

--- src/main/java/com/learn/RestWithDatabase/configs/JwtFilter.java
+++ src/main/java/com/learn/RestWithDatabase/configs/JwtFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,8 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learn.RestWithDatabase.dto.CommonResponse;
 import com.learn.RestWithDatabase.services.JWTService;
 import com.learn.RestWithDatabase.services.UserDetailsServiceImpl;
 
@@ -29,33 +32,50 @@ public class JwtFilter extends OncePerRequestFilter {
   @Autowired
   ApplicationContext context;
 
+  @Autowired
+  public ObjectMapper objectMapper;
+
   @SuppressWarnings("null")
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-    String authHeader = request.getHeader("Authorization");
-    String token = null;
-    String username = null;
+    try {
+      String authHeader = request.getHeader("Authorization");
+      String token = null;
+      String username = null;
 
-    if (authHeader != null && authHeader.startsWith("Bearer ")) {
-      token = authHeader.substring(7);
-      username = jwtService.extractUsername(token);
-    }
-
-    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
-      UserDetails userDetails = context.getBean(UserDetailsServiceImpl.class).loadUserByUsername(username);
-
-      if (jwtService.validateToken(token, userDetails)) {
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-            userDetails, null, userDetails.getAuthorities());
-        usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-      } else {
-        throw new ExpiredJwtException(null, null, "JWT Token has expired");
+      if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        token = authHeader.substring(7);
+        username = jwtService.extractUsername(token);
       }
+
+      if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+        UserDetails userDetails = context.getBean(UserDetailsServiceImpl.class).loadUserByUsername(username);
+
+        if (jwtService.validateToken(token, userDetails)) {
+          UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+              userDetails, null, userDetails.getAuthorities());
+          usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+          SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        } else {
+          throw new ExpiredJwtException(null, null, "JWT Token has expired");
+        }
+      }
+      filterChain.doFilter(request, response);
+
+    } catch (ExpiredJwtException e) {
+      CommonResponse<String> errorResponse = new CommonResponse<>("JWT Token Expired", HttpStatus.UNAUTHORIZED.value());
+      response.setStatus(HttpStatus.UNAUTHORIZED.value());
+      response.setContentType("application/json");
+      response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+
+    } catch (Exception e) {
+      CommonResponse<String> errorResponse = new CommonResponse<>("Unexpected Error", HttpStatus.BAD_REQUEST.value());
+      response.setStatus(HttpStatus.BAD_REQUEST.value());
+      response.setContentType("application/json");
+      response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
-    filterChain.doFilter(request, response);
   }
 }
